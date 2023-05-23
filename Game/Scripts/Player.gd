e3c19ffdda60a9d8a08d645f92953b236060e5b0
Terraquad <65852537+terraquad@users.noreply.github.com>
2023-05-23 14:41:52 +0200

    Included remade assets; finished player controller

--- Game/Scripts/Player.gd
+++ Game/Scripts/Player.gd
@@ -1,8 +1,7 @@
 extends CharacterBody2D
 
-@export var move_speed_accel = 100.0
-@export var move_speed_max = 200.0
-@export var move_speed_decel = 50.0
+@export var move_speed: int
+@export var jump_move_speed: int
 
 @export_range(0, 500) var jump_height: float
 @export_range(0, 5) var jump_time_to_peak: float
@@ -15,10 +14,11 @@ extends CharacterBody2D
 
 var jumpAvailable: bool = true
 var jumpCounter: int = 0
+var currMoveSpeed: int
 
 func _process(delta):
 	velocity.y += get_gravity() * delta
-	velocity.x = get_input_velocity() * move_speed_max
+	velocity.x = get_input_velocity() * currMoveSpeed
 
 	if Input.is_action_just_pressed("jump") and jumpAvailable:
 		velocity.y = jump_velocity
@@ -29,7 +29,6 @@ func _process(delta):
 		$JumpVFX.emitting = false
 		velocity.y /= 2
 
-
 	move_and_slide()
 
 func _physics_process(_delta):
@@ -39,6 +38,12 @@ func _physics_process(_delta):
 	if is_on_floor(): # Reset jump on floor hit
 		jumpAvailable = true
 		jumpCounter = 0
+		currMoveSpeed = move_speed
+	else:
+		if jumpCounter == 1:
+			currMoveSpeed = jump_move_speed
+		else:
+			currMoveSpeed = (jump_move_speed - move_speed) / 3 + move_speed
 
 func get_gravity():
 	return jump_gravity if velocity.y < 0.0 else fall_gravity
@@ -47,8 +52,8 @@ func get_input_velocity():
 	var horizontal := 0.0
 	
 	if Input.is_action_pressed("move_left"):
-		horizontal -= move_speed_accel
+		horizontal -= 1.0
 	if Input.is_action_pressed("move_right"):
-		horizontal += move_speed_accel
+		horizontal += 1.0
 	
 	return horizontal
